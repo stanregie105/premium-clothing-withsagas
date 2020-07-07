d88e4f30b6f6eb8299ec430043615b9f7162bf0b
stanregie105 <stanleyregal004@yahoo.com>
2020-07-06 23:49:10 -0700

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React,{ Component}  from 'react';
+import HomePage from './homepage.component'
+import './App.css';
+//import CardList from './card';
+
+
+
+
+class App extends Component{
+
+  render(){ 
+    return (
+
+      <div>
+        <HomePage/>
+      </div>
+     );
+
+  }
+}
+
+
+export default App;
