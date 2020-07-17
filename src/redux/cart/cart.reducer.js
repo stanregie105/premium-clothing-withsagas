7ccb6aed7b81528c9f28e9ffa34370144156debc
stanregie105 <stanleyregal004@yahoo.com>
2020-07-17 11:15:10 -0700

    added add item and remove item quantity to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import cartActionTypes from './cart.types';
-import { addItemsToCart} from './cart.utils';
+import { addItemsToCart,removeItemFromCart} from './cart.utils';
 const INITIAL_STATE ={
     hidden: true,
     cartItems: []
@@ -11,12 +11,24 @@ const cartReducer =(state=INITIAL_STATE, action)=>{
        return{
            ...state,
            hidden: !state.hidden
-       }
+       };
+       case cartActionTypes.CLEAR_ITEM_FROM_CART:
+       return{
+           ...state,
+           cartItems: state.cartItems.filter(
+               cartItem=>cartItem.id!==action.payload.id
+               )
+       };
+       case cartActionTypes.REMOVE_ITEM:
+       return{
+           ...state,
+           cartItems:removeItemFromCart(state.cartItems,action.payload)
+       };
        case cartActionTypes.ADD_ITEM:
        return {
            ...state,
            cartItems:addItemsToCart(state.cartItems, action.payload)
-       }
+       };
 
        default:
          return state;
